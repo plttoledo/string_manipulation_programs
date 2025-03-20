126585e168dbc1bfa1d6fe38251beb19c36a6b45
plttoledo <toledopreciouslaine@gmail.com>
2025-03-20 20:11:25 +0800

    Added the code, used .upper() function to turn user input to all capital letters.

--- string_manipulation_programs/program_3.py
+++
@@ -1,2 +0,0 @@
-# Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
-# Get user input fullname, print input in capital letters. (.upper/upper())
---
+++ string_manipulation_programs/user_input_turn_all_capital_letters.py
@@ -0,0 +1,6 @@
+# Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+# Get user input fullname, print input in capital letters. (.upper/upper())
+
+user_input = input("Enter your fullname: ").upper()
+
+print(user_input)
