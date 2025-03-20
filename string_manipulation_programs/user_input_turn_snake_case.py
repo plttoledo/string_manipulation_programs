e936644b45a5858d1cedb455ad75871fe8c7c32b
plttoledo <toledopreciouslaine@gmail.com>
2025-03-20 21:23:37 +0800

    Added code, used .lower() to make the input lowercase, and .replce() to replace whitespace with underscore.

--- string_manipulation_programs/user_input_turn_snake_case.py
+++ string_manipulation_programs/user_input_turn_snake_case.py
@@ -1,3 +1,7 @@
 # Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 # Get user input fullname in incorrect casing, print in snake case (use .lower/lower() and .replace)
 
+user_input = input("Enter your fullname: ").lower()
+replace_space = user_input.replace(" ", "_")
+
+print(replace_space)
