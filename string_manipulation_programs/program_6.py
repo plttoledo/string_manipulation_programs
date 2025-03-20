af931eb482b0fd317b91a01bfd861df9f4d406ef
plttoledo <toledopreciouslaine@gmail.com>
2025-03-20 20:37:24 +0800

    Updated pseudocode, added the code, used .swapcase() function to swap the letters from uppercase to lowercase and vice versa.

--- string_manipulation_programs/program_6.py
+++
@@ -1,2 +0,0 @@
-# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
-# Get user input fullname in incorrect casing, print in reverse casing. (.reverse/reverse())
---
+++ string_manipulation_programs/user_input_swap_cases.py
@@ -0,0 +1,6 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+# Get user input fullname in incorrect casing, print in reverse casing. (.swapcase())
+
+user_input = input("Enter your fullname: ").swapcase()
+
+print(user_input)
