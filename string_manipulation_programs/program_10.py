28b3b09dfdd2100da5f99b6732f7da9450b462fa
plttoledo <toledopreciouslaine@gmail.com>
2025-03-20 21:19:18 +0800

    Removed an unnecessary remove import function.

--- string_manipulation_programs/program_10.py
+++ string_manipulation_programs/program_10.py
@@ -1,2 +1,3 @@
 # Prog10: Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 # Get user input fullname in incorrect casing, print in snake case (use .lower/lower() and .replace)
+
--- string_manipulation_programs/user_input_turn_pascal_case.py
+++ string_manipulation_programs/user_input_turn_pascal_case.py
@@ -1,6 +1,5 @@
 # Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 # Get user input fullname in incorrect casing, print in pascal case (use .replace to replace whitespace with "" and do similar thing to program 5.)
-from os import remove
 
 user_input = input("Enter your fullname: ").title()
 remove_space = user_input.replace(" ", "")
