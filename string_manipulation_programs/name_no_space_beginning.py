c21d535f7caac4fddae513edd1e310dddce5939a
plttoledo <toledopreciouslaine@gmail.com>
2025-03-20 19:52:02 +0800

    Added the .strip() code

--- string_manipulation_programs/name_no_space_beginning.py
+++ string_manipulation_programs/name_no_space_beginning.py
@@ -1,2 +1,6 @@
 # Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 # Get user with several spaces at the beginning, print removed spaces at the beginning.
+
+user_input = input("Enter your fullname: ").strip()
+
+print(user_input)
