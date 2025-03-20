5f4ca0337427e9413d310724c8ac17e983525e46
plttoledo <toledopreciouslaine@gmail.com>
2025-03-20 20:32:31 +0800

    Added code, used the .title() function to only capitalize the first letter and lowercase the following letters for each name.

--- string_manipulation_programs/program_5.py
+++
@@ -1,2 +0,0 @@
-# Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
-# User input full name in incorrect casing, print in correct casing (Capital in first letter for first, middle, and last name.)
