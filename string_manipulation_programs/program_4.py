bd9c49b2b7fac177dda63048d36a83008a4b96df
plttoledo <toledopreciouslaine@gmail.com>
2025-03-20 20:18:46 +0800

    Added code, used .lower() function to turn input into lowercase characters.

--- string_manipulation_programs/program_4.py
+++
@@ -1,2 +0,0 @@
-# Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
-# Get user input fullname, print input in lowercase letters. (.lower/lower())
